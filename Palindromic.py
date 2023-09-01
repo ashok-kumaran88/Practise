ec3c248a100d6cc38d0b2dff7917c12b4d913b54
ashok-kumaran88 <44286318+ashok-kumaran88@users.noreply.github.com>
2023-09-01 17:57:38 +0530

    Create Palindromic.py

---
+++ Palindromic.py
@@ -0,0 +1,19 @@
+def isPalindrome(num):
+
+    tmp_rev = 0
+    tmp_num = num
+    while tmp_num > 0:
+        
+        tmp_rem = tmp_num % 10
+        tmp_num = tmp_num // 10
+        tmp_rev =  tmp_rev * 10 + tmp_rem
+        print('Number = ',tmp_num, "Reminder = ", tmp_rem, "Reverse = ", tmp_rev )
+
+    if num == tmp_rev:
+        print(num," is Palindrome")
+    else:
+        print(num," is not Palindrome")
+        
+			    
+num = 155351
+isPalindrome(num)
