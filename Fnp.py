a8ae7389a82e36f97f045d4333d8f2071506db6c
ashok-kumaran88 <44286318+ashok-kumaran88@users.noreply.github.com>
2023-09-02 19:57:11 +0530

    Update Fnp.py

--- Fnp.py
+++ Fnp.py
@@ -1,8 +1,26 @@
-row = 11
-col = 6
+rows = 5
 
-for i in range(row):
-    for j in range(i+1):
-        print(j+1, end=" ")
-    print("\n")
+k = 0
+count=0
+count1=0
 
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print("0 ", end="")
+        count+=1
+    
+    while k!=((2*i)-1):
+        if count<=rows-1:
+            print(i+k, end=" ")
+            count+=1
+        else:
+            count1+=1
+            print(i+k-(2*count1), end=" ")
+        k += 1
+
+    for space in range(1, (rows-i)+1):
+        print("0 ", end="")
+        count+=1
+    
+    count1 = count = k = 0
+    print()
