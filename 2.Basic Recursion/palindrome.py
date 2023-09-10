45e3f5f32fbcd77c64f22be0b7c3d3682c3a4dbe
ashok-kumaran88 <44286318+ashok-kumaran88@users.noreply.github.com>
2023-09-11 02:13:13 +0530

    Updated

--- 2.Basic Recursion/palindrome.py
+++ 2.Basic Recursion/palindrome.py
@@ -9,6 +9,7 @@ class Solution(object):
         # If the start is not equal to the end, not the palindrome.
         if str[i]!=str[len(str)-i-1] : return False
 
+        #  print (i,':', str[i],':',  str[len(str)-i-1])
         # If both characters are the same, increment i and check start+1 and end-1.
         return Solution().palindrome_BF(str,i+1)
 
